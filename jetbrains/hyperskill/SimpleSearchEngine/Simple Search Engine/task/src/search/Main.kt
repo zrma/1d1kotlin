a349f6721d10217004839f7ed95f99f08c31ce36
zrma <bulbitain@gmail.com>
2021-06-12 14:05:10 +0900

    solve Stage 1/6: String theory

--- jetbrains/hyperskill/SimpleSearchEngine/Simple Search Engine/task/src/search/Main.kt
+++ jetbrains/hyperskill/SimpleSearchEngine/Simple Search Engine/task/src/search/Main.kt
@@ -1,5 +1,15 @@
 package search
 
 fun main() {
-    println("Hello, World!")
+  val inputStrings = readLine()
+  val targetString = readLine()
+
+  inputStrings?.split(" ")?.let { ss ->
+    val idx = ss.indexOf(targetString)
+    if (idx >= 0) {
+      print(idx + 1)
+    } else {
+      print("Not found")
+    }
+  }
 }
