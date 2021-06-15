0e42ad36279335e489b9018c5c3f96f72509a994
zrma <bulbitain@gmail.com>
2021-06-15 20:26:59 +0900

    solve Topics: Standard input

---
+++ JetBrainsAcademy/Topics/Standard input/Four numbers/src/Main.kt
@@ -0,0 +1,8 @@
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    while (scanner.hasNext()) {
+        println(scanner.nextInt())
+    }
+}
---
+++ JetBrainsAcademy/Topics/Standard input/Read and print/src/Main.kt
@@ -0,0 +1,6 @@
+import java.util.*
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    scanner.forEach { println(it) }
+}
