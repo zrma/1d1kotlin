5d43c9410ac05e046932ecacd176d09750a8af69
zrma <bulbitain@gmail.com>
2021-12-05 17:47:21 +0900

    solve Topics: Default arguments additional contents

---
+++ JetBrainsAcademy/Topics/Default arguments/Polynomial/src/Main.kt
@@ -0,0 +1,4 @@
+@Suppress("unused")
+fun polynomial(x: Int, c: Int, b: Int = 0, a: Int = 0): Int {
+    return a * x * x + b * x + c
+}
---
+++ JetBrainsAcademy/Topics/Default arguments/Summation/src/Main.kt
@@ -0,0 +1,4 @@
+@Suppress("unused")
+fun sum5(a1: Int = 0, a2: Int = 0, a3: Int = 0, a4: Int = 0, a5: Int = 0): Int {
+    return a1 + a2 + a3 + a4 + a5
+}
