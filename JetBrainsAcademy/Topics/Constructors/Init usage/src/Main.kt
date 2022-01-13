f7d32f7de64f372fe61a81a35ef702fef95d2f4c
zrma <bulbitain@gmail.com>
2022-01-13 23:41:05 +0900

    solve Topics: Constructors additional contents

---
+++ JetBrainsAcademy/Topics/Constructors/Init usage/src/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val timerValue = readLine()!!.toInt()
+    val timer = ByteTimer(timerValue)
+    println(timer.time)
+}
+
+class ByteTimer(var time: Int) {
+    init {
+        time = time.coerceIn(-128..127)
+    }
+}
