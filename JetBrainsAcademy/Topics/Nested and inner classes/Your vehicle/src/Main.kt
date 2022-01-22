28cd99fed0ad9bb839125a19a8e0f5ee8236d452
zrma <bulbitain@gmail.com>
2022-01-22 15:28:44 +0900

    solve Topics: Nested and inner classes additional contents

---
+++ JetBrainsAcademy/Topics/Nested and inner classes/Your vehicle/src/Main.kt
@@ -0,0 +1,12 @@
+class Vehicle {
+    inner class Engine {
+        fun start() {
+            println("RRRrrrrrrr....")
+        }
+    }
+}
+// do not touch the class above
+
+fun main() {
+    Vehicle().Engine().start()
+}
