30f4b0777b06779560ca8e302f63be2c01d059a5
zrma <bulbitain@gmail.com>
2022-01-29 22:53:28 +0900

    update task-remote-info.yaml

--- JetBrainsAcademy/Topics/The main() function/Printing arguments/src/Main.kt
+++ JetBrainsAcademy/Topics/The main() function/Printing arguments/src/Main.kt
@@ -1,9 +1,3 @@
 fun main(args: Array<String>) {
-    if (args.size != 3) {
-        println("Invalid number of program arguments")
-        return
-    }
-    args.withIndex().forEach {
-        println("Argument ${it.index.inc()}: ${it.value}. It has ${it.value.length} characters")
-    }
+    TODO()
 }
