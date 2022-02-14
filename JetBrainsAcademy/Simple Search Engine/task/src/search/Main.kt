016d6161749c71583e0508888d7858bc71c4651d
zrma <bulbitain@gmail.com>
2022-02-14 23:39:58 +0900

    solve Stage 6/6: Search strategies in Simple Search Engine

--- JetBrainsAcademy/Simple Search Engine/task/src/search/Main.kt
+++ JetBrainsAcademy/Simple Search Engine/task/src/search/Main.kt
@@ -1,6 +1,7 @@
 package search
 
 import java.io.File
+import java.util.*
 
 fun main(args: Array<String>) {
     val dataFile = args[1]
@@ -10,11 +11,47 @@ fun main(args: Array<String>) {
     while (true) {
         when (readLine()!!.toInt()) {
             1 -> {
-                val target = readLine()!!
-                if (target in index) {
-                    index[target]!!.forEach { println(data[it]) }
-                } else {
-                    println("No matching people found.")
+                val strategy = readLine()!!
+                val targets = readLine()!!.split(" ")
+
+                when (strategy) {
+                    "ALL" -> {
+                        var results = index.values.flatten().toSet()
+                        for (target in targets) {
+                            if (target !in index) {
+                                results = emptySet()
+                                break
+                            }
+                            results = results.intersect(index[target]!!.toSet())
+                        }
+                        for (result in results) {
+                            println(data[result])
+                        }
+                    }
+                    "ANY" -> {
+                        var results = emptySet<Int>()
+                        for (target in targets) {
+                            if (target !in index) {
+                                continue
+                            }
+                            results = results.union(index[target]!!.toSet())
+                        }
+                        for (result in results) {
+                            println(data[result])
+                        }
+                    }
+                    else -> {
+                        var results = index.values.flatten().toSet()
+                        for (target in targets) {
+                            if (target !in index) {
+                                continue
+                            }
+                            results = results.subtract(index[target]!!.toSet())
+                        }
+                        for (result in results) {
+                            println(data[result])
+                        }
+                    }
                 }
             }
             2 -> {
@@ -33,8 +70,8 @@ fun main(args: Array<String>) {
     }
 }
 
-fun buildIndex(data: List<String>): Map<String, List<Int>> {
-    val result = mutableMapOf<String, List<Int>>()
+fun buildIndex(data: List<String>): TreeMap<String, List<Int>> {
+    val result = TreeMap<String, List<Int>>(String.CASE_INSENSITIVE_ORDER)
     for ((index, datum) in data.withIndex()) {
         for (word in datum.split(" ")) {
             result[word] = result.getOrDefault(word, listOf()) + index
