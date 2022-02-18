0a6eeeb358cb342e4de03582f7f0fc0e703cbcda
zrma <bulbitain@gmail.com>
2022-02-18 22:27:54 +0900

    solve Stage 2/5: Game loop

--- JetBrainsAcademy/Pawns-Only Chess/Pawns-Only Chess/task/test/PawnsOnlyChessTest.kt
+++ JetBrainsAcademy/Pawns-Only Chess/Pawns-Only Chess/task/test/PawnsOnlyChessTest.kt
@@ -8,16 +8,162 @@ val pawnsBlack = List<Pair<Int, Int>>(8) {index -> Pair(6,index) }
 
 class PawnsOnlyChessTest : StageTest<Any>() {
     @DynamicTest
-    fun chessboardTest(): CheckResult {
+    fun test1(): CheckResult {
         val main = TestedProgram()
-        val outputString = main.start().trim()
+        var outputString = main.start().trim()
+
         var position = checkOutput(outputString.toLowerCase(), 0, "pawns-only chess")
-        if ( position  == -1 ) return CheckResult(false, "Missing program title")
-        position = checkChessboard(outputString, position, pawnsWhite, pawnsBlack)
+        if ( position  == -1 ) return CheckResult(false, "Program title is expected.")
+        position = checkOutput(outputString.toLowerCase(), position, "first player's name:")
+        if ( position  == -1 ) return CheckResult(false, "Player 1 name prompt is expected.")
+
+        outputString = main.execute("John").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "second player's name:")
+        if ( position  == -1 ) return CheckResult(false, "Player 2 name prompt is expected.")
+
+        outputString = main.execute("Amelia").trim()
+        position = checkChessboard(outputString, 0, pawnsWhite, pawnsBlack)
         if ( position  == -1 ) return CheckResult(false, "Wrong chessboard printout")
+        position = checkOutput(outputString.toLowerCase(), position, "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Player 1 prompt to play is expected.")
+
+        outputString = main.execute("a1h8").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "amelia's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Player 2 prompt to play is expected.")
+
+        outputString = main.execute("h1a8").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Player 1 prompt to play is expected.")
+
+        outputString = main.execute("e2e4").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "amelia's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Player 2 prompt to play is expected.")
+
+        outputString = main.execute("e7e5").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Player 1 prompt to play is expected.")
+
+        outputString = main.execute("exit").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "bye")
+        if ( position  == -1 ) return CheckResult(false, "Exit message is expected.")
 
         return CheckResult.correct()
     }
+
+    @DynamicTest
+    fun test2(): CheckResult {
+        val main = TestedProgram()
+        var outputString = main.start().trim()
+
+        var position = checkOutput(outputString.toLowerCase(), 0, "pawns-only chess")
+        if ( position  == -1 ) return CheckResult(false, "Program title is expected.")
+        position = checkOutput(outputString.toLowerCase(), position, "first player's name:")
+        if ( position  == -1 ) return CheckResult(false, "Player 1 name prompt is expected.")
+
+        outputString = main.execute("John").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "second player's name:")
+        if ( position  == -1 ) return CheckResult(false, "Player 2 name prompt is expected.")
+
+        outputString = main.execute("Amelia").trim()
+        position = checkChessboard(outputString, 0, pawnsWhite, pawnsBlack)
+        if ( position  == -1 ) return CheckResult(false, "Wrong chessboard printout")
+        position = checkOutput(outputString.toLowerCase(), position, "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Player 1 prompt to play is expected.")
+
+        outputString = main.execute("a0h8").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("a1h9").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("a1i8").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("e2e4").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "amelia's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Player 2 prompt to play is expected.")
+
+        outputString = main.execute("a2a3a").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "amelia's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("exit").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "bye")
+        if ( position  == -1 ) return CheckResult(false, "Exit message is expected.")
+
+        return CheckResult.correct()
+    }
+
+    @DynamicTest
+    fun testAdd1(): CheckResult {
+        val main = TestedProgram()
+        var outputString = main.start().trim()
+
+        var position = checkOutput(outputString.toLowerCase(), 0, "pawns-only chess")
+        if ( position  == -1 ) return CheckResult(false, "Program title is expected.")
+        position = checkOutput(outputString.toLowerCase(), position, "first player's name:")
+        if ( position  == -1 ) return CheckResult(false, "Player 1 name prompt is expected.")
+
+        outputString = main.execute("John").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "second player's name:")
+        if ( position  == -1 ) return CheckResult(false, "Player 2 name prompt is expected.")
+
+        outputString = main.execute("Amelia").trim()
+        position = checkChessboard(outputString, 0, pawnsWhite, pawnsBlack)
+        if ( position  == -1 ) return CheckResult(false, "Wrong chessboard printout")
+        position = checkOutput(outputString.toLowerCase(), position, "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Player 1 prompt to play is expected.")
+
+        outputString = main.execute("j1h8").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("m1h8").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("a1n8").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("a1z8").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("a0a8").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("a7a9").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("a10a8").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("a1a18").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("a2 a4").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("a2\ta4").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "invalid input", "john's turn:")
+        if ( position  == -1 ) return CheckResult(false, "Incorrect output after an invalid command.")
+
+        outputString = main.execute("exit").trim()
+        position = checkOutput(outputString.toLowerCase(), 0, "bye")
+        if ( position  == -1 ) return CheckResult(false, "Exit message is expected.")
+
+        return CheckResult.correct()
+    }
+
 }
 
 fun checkChessboard(outputString: String, searchPos: Int, pawnsWhite: List<Pair<Int, Int>>, pawnsBlack: List<Pair<Int, Int>>): Int {
