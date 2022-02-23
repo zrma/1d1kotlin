995704717cb2d58c6e7e4ff88b7f6df8908f9442
zrma <bulbitain@gmail.com>
2022-02-23 21:54:49 +0900

    solve Stage 1/4: Convert decimals

--- JetBrainsAcademy/Number Base Converter/task/src/converter/Main.kt
+++ JetBrainsAcademy/Number Base Converter/task/src/converter/Main.kt
@@ -1,5 +1,21 @@
 package converter
 
 fun main() {
-    println("Hello, world!")
+    print("Enter number in decimal system:")
+    val decimalNumber = readLine()!!.toInt()
+
+    print("Enter target base:")
+    val targetBase = readLine()!!.toInt()
+
+    println("Conversion result: ${convert(decimalNumber, targetBase)}")
+}
+
+fun convert(decimalNumber: Int, targetBase: Int): String {
+    val digits = mutableListOf<Int>()
+    var number = decimalNumber
+    while (number > 0) {
+        digits.add(number % targetBase)
+        number /= targetBase
+    }
+    return digits.reversed().joinToString("") { it.toString() }
 }
