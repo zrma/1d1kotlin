fdf0637816eafc3953dc7d5107e3b2c2ad71e1e0
zrma <bulbitain@gmail.com>
2022-03-01 00:29:44 +0900

    solve Stage 4/4: Convert fractions

--- JetBrainsAcademy/Number Base Converter/task/src/converter/Main.kt
+++ JetBrainsAcademy/Number Base Converter/task/src/converter/Main.kt
@@ -1,7 +1,8 @@
 package converter
 
+import java.math.BigDecimal
 import java.math.BigInteger
-import kotlin.math.pow
+import java.math.RoundingMode
 
 fun main() {
     while (true) {
@@ -21,13 +22,25 @@ fun convertLoop(source: Int, target: BigInteger) {
         val number = readLine()
         if (number == null || number == "/back") return
 
-        val decimal = convertToDecimal(number, source)
-        val result = convertFromDecimal(decimal, target)
-        println("Conversion result: $result")
+        if (number.contains(".")) {
+            val (integer, fraction) = number.split(".")
+
+            val decimalInteger = toDecimalInteger(integer, source)
+            val resultInteger = fromDecimalInteger(decimalInteger, target)
+
+            val decimalFraction = toDecimalFraction(fraction, source)
+            val fromDecimalFraction = fromDecimalFraction(decimalFraction, target.toBigDecimal())
+
+            println("Conversion result: $resultInteger.$fromDecimalFraction")
+        } else {
+            val decimal = toDecimalInteger(number, source)
+            val result = fromDecimalInteger(decimal, target)
+            println("Conversion result: $result")
+        }
     }
 }
 
-fun convertFromDecimal(decimalNumber: BigInteger, targetBase: BigInteger): String {
+fun fromDecimalInteger(decimalNumber: BigInteger, targetBase: BigInteger): String {
     val digits = mutableListOf<Char>()
     var number = decimalNumber
     while (number > BigInteger.ZERO) {
@@ -39,18 +52,54 @@ fun convertFromDecimal(decimalNumber: BigInteger, targetBase: BigInteger): Strin
         }
         number /= targetBase
     }
+    if (digits.isEmpty()) {
+        return "0"
+    }
     return digits.reversed().joinToString("") { it.toString() }
 }
 
-fun convertToDecimal(sourceNumber: String, sourceBase: Int): BigInteger {
-    var decimalNumber = BigInteger.ZERO
-    var power = 0
-    for (digit in sourceNumber.reversed()) {
-        val cur = charToInt(digit).toBigInteger() * sourceBase.toBigInteger().pow(power)
-        decimalNumber = decimalNumber.add(cur)
-        power++
+fun toDecimalInteger(sourceNumber: String, sourceBase: Int): BigInteger {
+    var result = BigInteger.ZERO
+    for (digit in sourceNumber) {
+        result = result.multiply(BigInteger.valueOf(sourceBase.toLong()))
+        result = result.add(charToInt(digit).toBigInteger())
     }
-    return decimalNumber
+    return result
+}
+
+fun fromDecimalFraction(decimalNumber: BigDecimal, targetBase: BigDecimal): String {
+    val digits = mutableListOf<Char>()
+    var number = decimalNumber
+    while (number > BigDecimal.ZERO && digits.size < 5) {
+        number *= targetBase
+        val digit = number.toInt()
+        if (digit == 0) {
+            digits.add('0')
+            continue
+        }
+        number -= digit.toBigDecimal()
+        if (digit < 10) {
+            digits.add(digit.digitToChar())
+        } else {
+            digits.add('a' + (digit - 10))
+        }
+    }
+    if (digits.size < 5) {
+        repeat(5 - digits.size) {
+            digits.add('0')
+        }
+    }
+    return digits.joinToString("") { it.toString() }
+}
+
+fun toDecimalFraction(sourceNumber: String, sourceBase: Int): BigDecimal {
+    var result = BigDecimal.ZERO
+    var base = BigDecimal(1.0)
+    for (digit in sourceNumber) {
+        base = base.divide(BigDecimal(sourceBase.toLong()), 10, RoundingMode.CEILING)
+        result = result.add(charToInt(digit).toBigDecimal().multiply(base))
+    }
+    return result
 }
 
 fun charToInt(char: Char): Int {
