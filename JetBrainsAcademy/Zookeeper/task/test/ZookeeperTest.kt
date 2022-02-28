457dba61cfc650f20e397762a6bb5a7547aca0f8
zrma <bulbitain@gmail.com>
2022-03-01 01:04:41 +0900

    add skeleton: Stage 1/4: Rush into print

---
+++ JetBrainsAcademy/Zookeeper/task/test/ZookeeperTest.kt
@@ -0,0 +1,27 @@
+import org.hyperskill.hstest.dynamic.DynamicTest
+import org.hyperskill.hstest.stage.StageTest
+import org.hyperskill.hstest.testcase.CheckResult
+import org.hyperskill.hstest.testing.TestedProgram
+
+class ZookeeperTest : StageTest<String>() {
+
+    private val correctOutput = """
+        I love animals!
+        Let's check on the animals...
+        The deer looks fine.
+        The bat looks happy.
+        The lion looks healthy.
+        """.trimIndent()
+
+    @DynamicTest
+    fun test(): CheckResult {
+        val testedProgram = TestedProgram()
+        val output = testedProgram.start().trim()
+
+        if (output != correctOutput) {
+            return CheckResult.wrong("Your output should be like in the example!")
+        }
+
+        return CheckResult.correct()
+    }
+}
