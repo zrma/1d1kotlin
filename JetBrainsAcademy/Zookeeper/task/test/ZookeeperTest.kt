2353bc306fca7a6ccefee6b8d3fbf42a98ff46ff
zrma <bulbitain@gmail.com>
2022-03-01 01:14:31 +0900

    solve Stage 3/4: What's inside

--- JetBrainsAcademy/Zookeeper/task/test/ZookeeperTest.kt
+++ JetBrainsAcademy/Zookeeper/task/test/ZookeeperTest.kt
@@ -3,6 +3,7 @@ import org.hyperskill.hstest.stage.StageTest
 import org.hyperskill.hstest.testcase.CheckResult
 import org.hyperskill.hstest.testing.TestedProgram
 
+
 class ZookeeperTest : StageTest<String>() {
 
     val camel = """Switching on the camera in the camel habitat...
@@ -25,13 +26,129 @@ class ZookeeperTest : StageTest<String>() {
            /_I      L_I   L_I     /_I
 Look at that! Our little camel is sunbathing!"""
 
-    @DynamicTest
-    fun test(): CheckResult {
-        val testedProgram = TestedProgram();
-        val output = testedProgram.start().trim();
+    val lion = """Switching on the camera in the lion habitat...
+                                               ,w.
+                                             ,YWMMw  ,M  ,
+                        _.---.._   __..---._.'MMMMMw,wMWmW,
+                   _.-""        '''           YP"WMMMMMMMMMb,
+                .-' __.'                   .'     MMMMW^WMMMM;
+    _,        .'.-'"; `,       /`     .--""      :MMM[==MWMW^;
+ ,mM^"     ,-'.'   /   ;      ;      /   ,       MMMMb_wMW"  @\
+,MM:.    .'.-'   .'     ;     `\    ;     `,     MMMMMMMW `"=./`-,
+WMMm__,-'.'     /      _.\      F'''-+,,   ;_,_.dMMMMMMMM[,_ / `=_}
+"^MP__.-'    ,-' _.--""   `-,   ;       \  ; ;MMMMMMMMMMW^``; __|
+           /   .'            ; ;         )  )`{  \ `"^W^`,   \  :
+          /  .'             /  (       .'  /     Ww._     `.  `"
+         /  Y,              `,  `-,=,_{   ;      MMMP`""-,  `-._.-,
+        (--, )                `,_ / `) \/"")      ^"      `-, -;"\:
+The lion is roaring!"""
 
-        if (output != camel) {
-            return CheckResult.wrong("You should output a camel!");
+    val deer = """Switching on the camera in the deer habitat...
+   /|       |\
+`__\\       //__'
+   ||      ||
+ \__`\     |'__/
+   `_\\   //_'
+   _.,:---;,._
+   \_:     :_/
+     |@. .@|
+     |     |
+     ,\.-./ \
+     ;;`-'   `---__________-----.-.
+     ;;;                         \_\
+     ';;;                         |
+      ;    |                      ;
+       \   \     \        |      /
+        \_, \    /        \     |\
+          |';|  |,,,,,,,,/ \    \ \_
+          |  |  |           \   /   |
+          \  \  |           |  / \  |
+           | || |           | |   | |
+           | || |           | |   | |
+           | || |           | |   | |
+           |_||_|           |_|   |_|
+          /_//_/           /_/   /_/
+Our 'Bambi' looks hungry. Let's go to feed it!"""
+
+    val goose = """Switching on the camera in the goose habitat...
+
+                                    _
+                                ,-"" "".
+                              ,'  ____  `.
+                            ,'  ,'    `.  `._
+   (`.         _..--.._   ,'  ,'        \    \
+  (`-.\    .-""        ""'   /          (  d _b
+ (`._  `-"" ,._             (            `-(   \
+ <_  `     (  <`<            \              `-._\
+  <`-       (__< <           :
+   (__        (_<_<          ;
+    `------------------------------------------
+The goose is staring intently at you... Maybe it's time to change the channel?"""
+
+    val bat = """Switching on the camera in the bat habitat...
+_________________               _________________
+ ~-.              \  |\___/|  /              .-~
+     ~-.           \ / o o \ /           .-~
+        >           \\  W  //           <
+       /             /~---~\             \
+      /_            |       |            _\
+         ~-.        |       |        .-~
+            ;        \     /        i
+           /___      /\   /\      ___\
+                ~-. /  \_/  \ .-~
+                   V         V
+This bat looks like it's doing fine."""
+
+    val rabbit = """Switching on the camera in the rabbit habitat...
+         ,
+        /|      __
+       / |   ,-~ /
+      Y :|  //  /
+      | jj /( .^
+      >-"~"-v"
+     /       Y
+    jo  o    |
+   ( ~T~     j
+    >._-' _./
+   /   "~"  |
+  Y     _,  |
+ /| ;-"~ _  l
+/ l/ ,-"~    \
+\//\/      .- \
+ Y        /    Y
+ l       I     !
+ ]\      _\    /"\
+(" ~----( ~   Y.  )
+It looks like we will soon have more rabbits!"""
+
+    private var theEndMessage = "---\nYou've reached the end of the program. To check another habitat, please restart the watcher."
+    private var indexArray = arrayOf("0", "1", "2", "3", "4", "5")
+    private var animalIndex = mapOf(
+            "0" to arrayOf(camel, "camel"),
+            "1" to arrayOf(lion, "lion"),
+            "2" to arrayOf(deer, "deer"),
+            "3" to arrayOf(goose, "goose"),
+            "4" to arrayOf(bat, "bat"),
+            "5" to arrayOf(rabbit, "rabbit")
+    )
+
+    @DynamicTest(data = "indexArray")
+    fun test(index: String): CheckResult {
+
+        val testedProgram = TestedProgram()
+        testedProgram.start()
+
+        val output = testedProgram.execute(index)
+
+        val animalImage = animalIndex[index]?.get(0).toString()
+        val animalName = animalIndex[index]?.get(1).toString()
+
+        if (!output.contains(animalImage)) {
+            return CheckResult.wrong("You should output a $animalName when the input is the number $index")
+        }
+
+        if (!output.contains(theEndMessage)) {
+            return CheckResult.wrong("You should output the message about the end of the program!")
         }
 
         return CheckResult.correct()
