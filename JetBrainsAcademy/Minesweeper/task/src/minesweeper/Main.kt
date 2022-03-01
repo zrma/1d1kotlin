c78e2647b950dc56e4dd3cc6e659398aa72b1d2b
zrma <bulbitain@gmail.com>
2022-03-01 19:31:03 +0900

    solve Stage 1/5: Lay the groundwork

--- JetBrainsAcademy/Minesweeper/task/src/minesweeper/Main.kt
+++ JetBrainsAcademy/Minesweeper/task/src/minesweeper/Main.kt
@@ -1,5 +1,36 @@
 package minesweeper
 
+const val MINE = "X"
+const val CELL = "."
+
 fun main() {
-    print("Hello, World!")
+    val board = newBoard(
+        arrayOf(
+            arrayOf(CELL, CELL, CELL, CELL, CELL, MINE, CELL, CELL, CELL),
+            arrayOf(CELL, CELL, CELL, CELL, CELL, CELL, MINE, CELL, CELL),
+            arrayOf(CELL, CELL, MINE, CELL, CELL, CELL, CELL, CELL, MINE),
+            arrayOf(CELL, CELL, CELL, MINE, CELL, CELL, CELL, CELL, CELL),
+            arrayOf(CELL, CELL, CELL, CELL, MINE, CELL, CELL, MINE, CELL),
+            arrayOf(CELL, CELL, CELL, CELL, CELL, CELL, CELL, CELL, CELL),
+            arrayOf(CELL, CELL, CELL, CELL, CELL, CELL, CELL, CELL, CELL),
+            arrayOf(MINE, CELL, CELL, CELL, CELL, CELL, MINE, CELL, CELL),
+            arrayOf(CELL, MINE, CELL, CELL, CELL, CELL, CELL, CELL, CELL),
+        )
+    )
+    printBoard(board)
+}
+
+class Cell(val data: String) {}
+
+fun newBoard(input: Array<Array<String>>): Array<Array<Cell>> {
+    return input.map { row -> row.map { col -> Cell(col) }.toTypedArray() }.toTypedArray()
+}
+
+fun printBoard(board: Array<Array<Cell>>) {
+    board.forEach { row ->
+        row.forEach { col ->
+            print(col.data)
+        }
+        println()
+    }
 }
