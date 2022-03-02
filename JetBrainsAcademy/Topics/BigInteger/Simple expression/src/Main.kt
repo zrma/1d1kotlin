b9c78ba2b0f585f16eb249286d651185c163ac89
zrma <bulbitain@gmail.com>
2022-03-02 21:24:08 +0900

    solve Topics: BigInteger additional contents

---
+++ JetBrainsAcademy/Topics/BigInteger/Simple expression/src/Main.kt
@@ -0,0 +1,10 @@
+import java.math.BigInteger
+
+fun main() {
+    val a = BigInteger(readLine()!!)
+    val b = BigInteger(readLine()!!)
+    val c = BigInteger(readLine()!!)
+    val d = BigInteger(readLine()!!)
+
+    println(-a * b + c - d)
+}
