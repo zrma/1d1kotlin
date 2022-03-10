ab9f03f6c46347bcce6cc5ad0e17a6df6ba34ab2
zrma <bulbitain@gmail.com>
2022-03-10 21:56:27 +0900

    solve Topics: Multi-dimensional array additional contents

---
+++ JetBrainsAcademy/Topics/Multi-dimensional array/Kotlin arrays/src/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var inputArray: Array<Array<String>> = arrayOf()
+    val n = readLine()!!.toInt()
+    for (i in 0 until n) {
+        val strings = readLine()!!.split(' ').toTypedArray()
+        inputArray += strings
+    }
+    // Do not change lines above
+    // Write your code here
+    println(inputArray[2].joinToString(", "))
+}
