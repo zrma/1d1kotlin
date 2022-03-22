aaebb27cdd5eac67e4f577a5a065139c8cce4495
zrma <bulbitain@gmail.com>
2022-03-22 21:06:56 +0900

    solve Stage 2/6: Negative photo

--- JetBrainsAcademy/Seam Carving/task/src/seamcarving/Main.kt
+++ JetBrainsAcademy/Seam Carving/task/src/seamcarving/Main.kt
@@ -5,22 +5,40 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
-fun main() {
-    println("Enter rectangle width:")
-    val width = readLine()!!.toInt()
+fun main(args: Array<String>) {
+    // use -in and -out to specify input and output files
+    if (args.size < 4) {
+        println("Usage: java Main -in input.png -out output.png")
+        return
+    }
+    val inputFile = File(args[1])
+    val outputFile = File(args[3])
 
-    println("Enter rectangle height:")
-    val height = readLine()!!.toInt()
+    // read image
+    val image = ImageIO.read(inputFile)
 
-    println("Enter output image name:")
-    val outputImageName = readLine()!!
+    // invert rgb of image
+    val rgbReverseImage = invertRGB(image)
 
-    val image = BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR)
+    // save reversed image
+    ImageIO.write(rgbReverseImage, "png", outputFile)
+}
 
-    val graphics = image.createGraphics()
-    graphics.color = Color.RED
-    graphics.drawLine(0, 0, width - 1, height - 1)
-    graphics.drawLine(width - 1, 0, 0, height - 1)
+fun invertRGB(image: BufferedImage): BufferedImage {
+    val width = image.width
+    val height = image.height
+    val newImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
 
-    ImageIO.write(image, "png", File(outputImageName))
+    for (x in 0 until width) {
+        for (y in 0 until height) {
+            newImage.setRGB(x, y, invertColor(image.getRGB(x, y)))
+        }
+    }
+    return newImage
+}
+
+fun invertColor(rgb: Int): Int {
+    val color = Color(rgb)
+    val newColor = Color(255 - color.red, 255 - color.green, 255 - color.blue)
+    return newColor.rgb
 }
