a740013ae948069820a2f6518e074979ca5d6341
zrma <bulbitain@gmail.com>
2022-03-25 22:28:37 +0900

    update course-remote-info.yaml

--- JetBrainsAcademy/Seam Carving/task/test/SeamCarvingTest.java
+++ JetBrainsAcademy/Seam Carving/task/test/SeamCarvingTest.java
@@ -2,15 +2,15 @@ import org.apache.commons.codec.binary.Hex;
 import org.hyperskill.hstest.testcase.TestCase;
 import org.hyperskill.hstest.stage.StageTest;
 import org.hyperskill.hstest.testcase.CheckResult;
-
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -21,22 +21,120 @@ class CheckFailException extends Exception {
     }
 }
 
+class Image {
+    private BufferedImage image;
+
+    Image(String filename) throws IOException {
+        image = ImageIO.read(new File(filename));
+    }
+
+    private int dx2(int x, int y) {
+        if (x == 0) {
+            return dx2(x + 1, y);
+        }
+        else if (x == image.getWidth() - 1) {
+            return dx2(x - 1, y);
+        }
+        else {
+            Color l = new Color(image.getRGB(x - 1, y));
+            Color r = new Color(image.getRGB(x + 1, y));
+            return (l.getRed() - r.getRed()) * (l.getRed() - r.getRed()) +
+                (l.getGreen() - r.getGreen()) * (l.getGreen() - r.getGreen()) +
+                (l.getBlue() - r.getBlue()) * (l.getBlue() - r.getBlue());
+        }
+    }
+
+    private int dy2(int x, int y) {
+        if (y == 0) {
+            return dy2(x, y + 1);
+        }
+        else if (y == image.getHeight() - 1) {
+            return dy2(x, y - 1);
+        }
+        else {
+            Color t = new Color(image.getRGB(x, y - 1));
+            Color b = new Color(image.getRGB(x, y + 1));
+            return (t.getRed() - b.getRed()) * (t.getRed() - b.getRed()) +
+                (t.getGreen() - b.getGreen()) * (t.getGreen() - b.getGreen()) +
+                (t.getBlue() - b.getBlue()) * (t.getBlue() - b.getBlue());
+        }
+    }
+
+    public double pixelEnergy(int x, int y) {
+        return Math.sqrt((double)dx2(x, y) + (double)dy2(x, y));
+    }
+
+    public List<Integer> findVerticalSeam() throws CheckFailException {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (int y = 0; y < image.getHeight(); y++) {
+            boolean found = false;
+
+            for (int x = 0; x < image.getWidth(); x++) {
+                Color c = new Color(image.getRGB(x, y));
+                if (c.equals(Color.RED)) {
+                    result.add(x);
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+                throw new CheckFailException(String.format("Could not find seam pixel in row (%d)", y));
+        }
+        return result;
+    }
+}
+
 class OutFile {
     String hash;
-    String filename;
+    String inFileName;
+    String outFilename;
     int width;
     int height;
 
-    OutFile(String filename, int width, int height, String hash) {
-        this.filename = filename;
+    Double seamSum;
+
+    OutFile(String inFileName, String outFilename, int width, int height, String hash, Double sum) {
+        this.inFileName = inFileName;
+        this.outFilename = outFilename;
         this.width = width;
         this.height = height;
         this.hash = hash;
+        this.seamSum = sum;
+    }
+
+    public void checkSum() throws CheckFailException {
+        try {
+            double delta = 0.00001;
+
+            Image input = new Image(inFileName);
+            Image output = new Image(outFilename);
+
+            double actualSum = 0;
+            List<Integer> seam = output.findVerticalSeam();
+            for (int y = 0; y < seam.size(); y++) {
+                int x = seam.get(y);
+                actualSum += input.pixelEnergy(x, y);
+            }
+
+            if (actualSum < seamSum - delta || actualSum > seamSum + delta) {
+                throw new CheckFailException(
+                    String.format(
+                        "Total energy of your seam (%f) does not match expected value (%f +/- %f)",
+                        actualSum, seamSum, delta));
+            }
+
+        } catch (IOException e) {
+            throw new CheckFailException(
+                String.format(
+                    "Could not read output file '%s'. Please check you produce output file",
+                    outFilename));
+        }
     }
 
     public boolean compareWithActualMD5() throws CheckFailException {
         try {
-            File imgPath = new File(filename);
+            File imgPath = new File(outFilename);
             BufferedImage sourceImage = ImageIO.read(imgPath);
 
             BufferedImage rgbImage =
@@ -64,7 +162,7 @@ class OutFile {
             throw new CheckFailException(
                 String.format(
                     "Could not read output file '%s'. Please check you produce output file",
-                    filename));
+                    outFilename));
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
             throw new CheckFailException("Internal test error. please report to Hyperskill team");
@@ -75,13 +173,13 @@ class OutFile {
 
     public boolean compareActualDimensions() throws CheckFailException {
         try {
-            BufferedImage image = ImageIO.read(new File(filename));
+            BufferedImage image = ImageIO.read(new File(outFilename));
 
             if (image == null) {
                 throw new CheckFailException(
                     String.format(
                         "File format error. Looks like your output '%s' is not a valid image file.",
-                        filename));
+                        outFilename));
             }
 
             if (image.getWidth() != width) {
@@ -102,7 +200,7 @@ class OutFile {
             throw new CheckFailException(
                 String.format(
                     "Could not read output file '%s'. Please check you produce output file",
-                    filename));
+                    outFilename));
         }
 
         return true;
@@ -116,16 +214,22 @@ public class SeamCarvingTest extends StageTest<OutFile> {
 
         return Arrays.asList(
             new TestCase<OutFile>()
-                .addArguments("-in", "test/small.png", "-out", "test/small-energy.png")
-                .setAttach(new OutFile("test/small-energy.png", 15, 10, "931d2f37bb499ef6892db026f57525ba")),
+                .addArguments("-in", "test/small.png", "-out", "test/small-seam.png")
+                .setAttach(new OutFile("test/small.png", "test/small-seam.png",
+                    15, 10,
+                    "831b2b94d6ca3ae71a8aacec4e5b5e23", 466.371057)).setTimeLimit(-1),
 
             new TestCase<OutFile>()
-                .addArguments("-in", "test/blue.png", "-out", "test/blue-energy.png")
-                .setAttach(new OutFile("test/blue-energy.png", 500, 334, "0bdde2d55124785b16df005088f17e1a")),
+                .addArguments("-in", "test/blue.png", "-out", "test/blue-seam.png")
+                .setAttach(new OutFile("test/blue.png", "test/blue-seam.png",
+                    500, 334,
+                    "bfde19c2d97092e104b14fd07f2cf5f3", 1146.092943)),
 
             new TestCase<OutFile>()
-                .addArguments("-in", "test/trees.png", "-out", "test/trees-energy.png")
-                .setAttach(new OutFile("test/trees-energy.png", 600, 429, "89c4037e6c0b0de040d9fb85e4450ebc"))
+                .addArguments("-in", "test/trees.png", "-out", "test/trees-seam.png")
+                .setAttach(new OutFile("test/trees.png", "test/trees-seam.png",
+                    600, 429,
+                    "9298d037e2defc97a1b35e8abddafb02", 1788.013453))
         );
     }
 
@@ -133,7 +237,8 @@ public class SeamCarvingTest extends StageTest<OutFile> {
     public CheckResult check(String reply, OutFile expectedFile) {
         try {
             expectedFile.compareActualDimensions();
-            expectedFile.compareWithActualMD5();
+            //expectedFile.compareWithActualMD5();
+            expectedFile.checkSum();
         } catch (CheckFailException e) {
             return CheckResult.wrong(e.getMessage());
         }
