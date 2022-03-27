504c8b0c62e39f0efe490b1bebd51574980acb1a
zrma <bulbitain@gmail.com>
2022-03-27 22:22:11 +0900

    solve Topics: Data types additional contents

---
+++ JetBrainsAcademy/Topics/Data types/Edit it/src/Main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val part1 = "Kotlin "
+    val part2 = "is a cross-platform"
+    val part3 = ", statically typed, "
+    val part4 = "general-purpose programming language with type coercion."
+
+    print(part1)
+    print(part2)
+    print(part3)
+    print(part4)
+}
