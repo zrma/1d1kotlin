79917f585a2e6ab218d436556ab36f75e4f70b2b
zrma <bulbitain@gmail.com>
2022-04-16 21:42:33 +0900

    solve Topics: Multi-midensional array additional contents

---
+++ JetBrainsAcademy/Topics/Multi-dimensional array/Cube of zeros/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val array3D = arrayOf(
+        arrayOf(arrayOf(0, 0, 0), arrayOf(0, 0, 0), arrayOf(0, 0, 0)),
+        arrayOf(arrayOf(0, 0, 0), arrayOf(0, 0, 0), arrayOf(0, 0, 0)),
+        arrayOf(arrayOf(0, 0, 0), arrayOf(0, 0, 0), arrayOf(0, 0, 0))
+    )
+
+    println(array3D.contentDeepToString())
+}
