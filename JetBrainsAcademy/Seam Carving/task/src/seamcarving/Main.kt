9f6d7941261c087eea0dc0d61aa9a11c2ce2e95c
zrma <bulbitain@gmail.com>
2022-06-09 00:14:12 +0900

    solve Stage 4/6: Find the seam

--- JetBrainsAcademy/Seam Carving/task/src/seamcarving/Main.kt
+++ JetBrainsAcademy/Seam Carving/task/src/seamcarving/Main.kt
@@ -20,24 +20,86 @@ fun main(args: Array<String>) {
     val image = ImageIO.read(inputFile)
 
     val energyInfo = dualGradientEnergy(image)
-    val result = applyTable(image, energyInfo)
+    val normalized = normalize(image, energyInfo)
+
+    val seam = findSeam(normalized, energyInfo.first)
+
+    applySeam(image, seam)
 
     // save reversed image
-    ImageIO.write(result, "png", outputFile)
+    ImageIO.write(image, "png", outputFile)
 }
 
-fun applyTable(
-    image: BufferedImage,
-    energyInfo: Pair<Array<Array<Double>>, Double>
-): BufferedImage {
+fun applySeam(image: BufferedImage, seam: List<Pair<Int, Int>>) {
+    for ((y, x) in seam) {
+        image.setRGB(x, y, Color.RED.rgb)
+    }
+}
+
+fun findSeam(image: BufferedImage, energies: Array<Array<Double>>): List<Pair<Int, Int>> {
+    val width = image.width
+    val height = image.height
+
+    val sumMatrix = Array(height) { Array(width) { 0.0 } }
+
+    // fill sumMatrix from top to bottom
+    // each cell is sum of its energy and minimum energy of three neighbors in row above (up left,
+    // left, up right)
+    // https://en.m.wikipedia.org/wiki/Seam_carving#Dynamic_programming
+    for (y in 0 until height) {
+        for (x in 0 until width) {
+            if (y == 0) {
+                // top row has no row above it, so will be initialized with energy data
+                sumMatrix[y][x] = energies[y][x]
+            } else {
+                sumMatrix[y][x] = energies[y][x] + getNextMin(sumMatrix, x, y).first
+            }
+        }
+    }
+
+    val seam = emptyList<Pair<Int, Int>>().toMutableList()
+
+    // find minimum in last row and ...
+    var minimumEnergy = Double.MAX_VALUE
+    var xOfMinimum = 0
+    for (x in 0 until width) {
+        if (sumMatrix[height - 1][x] < minimumEnergy) {
+            minimumEnergy = sumMatrix[height - 1][x]
+            xOfMinimum = x
+        }
+    }
+    seam.add(Pair(height - 1, xOfMinimum))
+
+    // ... step back up each row, only up+left, up, up+right, always choose the minimum
+    for (y in height - 1 downTo 0) {
+        xOfMinimum += getNextMin(sumMatrix, xOfMinimum, y + 1).second
+
+        seam.add(Pair(y, xOfMinimum))
+    }
+
+    return seam
+}
+
+// get energy of three fields above field (w/h)
+// returns array with three values of top left, top, top right
+private fun getNextMin(sumMatrix: Array<Array<Double>>, x: Int, y: Int): Pair<Double, Int> {
+    val rightBorder = sumMatrix.first().size
+    return arrayOf(
+        Pair(if (x == 0) Double.MAX_VALUE else sumMatrix[y - 1][x - 1], -1),
+        Pair(sumMatrix[y - 1][x], 0),
+        Pair(if (x == rightBorder - 1) Double.MAX_VALUE else sumMatrix[y - 1][x + 1], 1)
+    ).minOfWith(compareBy { it.first }) { it }
+}
+
+fun normalize(image: BufferedImage, energyInfo: Pair<Array<Array<Double>>, Double>): BufferedImage {
     val width = image.width
     val height = image.height
     val newImage = BufferedImage(width, height, BufferedImage.TYPE_INT_RGB)
     val (energyTable, maxEnergyValue) = energyInfo
 
-    for (x in 0 until width) {
-        for (y in 0 until height) {
-            val energy = energyTable[x][y]
+    for (y in 0 until height) {
+        for (x in 0 until width) {
+            val energy = energyTable[y][x]
             val intensity = (255.0 * energy / maxEnergyValue).toInt()
             newImage.setRGB(x, y, Color(intensity, intensity, intensity).rgb)
         }
@@ -48,12 +110,12 @@ fun applyTable(
 fun dualGradientEnergy(image: BufferedImage): Pair<Array<Array<Double>>, Double> {
     val width = image.width
     val height = image.height
-    val res = Array(width) { Array(height) { 0.0 } }
+    val res = Array(height) { Array(width) { 0.0 } }
 
     var maxEnergyValue = 0.0
 
-    for (x in 0 until width) {
-        for (y in 0 until height) {
+    for (y in 0 until height) {
+        for (x in 0 until width) {
             val posX =
                 when (x) {
                     0 -> 1
@@ -72,16 +134,16 @@ fun dualGradientEnergy(image: BufferedImage): Pair<Array<Array<Double>>, Double>
             val down = Color(image.getRGB(x, posY + 1))
             val dX =
                 (left.red.toDouble() - right.red.toDouble()).pow(2.0) +
-                    (left.green.toDouble() - right.green.toDouble()).pow(2.0) +
-                    (left.blue.toDouble() - right.blue.toDouble()).pow(2.0)
+                        (left.green.toDouble() - right.green.toDouble()).pow(2.0) +
+                        (left.blue.toDouble() - right.blue.toDouble()).pow(2.0)
 
             val dY =
                 (up.red.toDouble() - down.red.toDouble()).pow(2.0) +
-                    (up.green.toDouble() - down.green.toDouble()).pow(2.0) +
-                    (up.blue.toDouble() - down.blue.toDouble()).pow(2.0)
+                        (up.green.toDouble() - down.green.toDouble()).pow(2.0) +
+                        (up.blue.toDouble() - down.blue.toDouble()).pow(2.0)
             val energy = sqrt(dX + dY)
             maxEnergyValue = maxOf(maxEnergyValue, energy)
-            res[x][y] = energy
+            res[y][x] = energy
         }
     }
     return res to maxEnergyValue
