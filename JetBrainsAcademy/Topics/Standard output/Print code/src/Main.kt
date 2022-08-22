59a473d829edc8751426e68d02749dc2a3d53f1b
zrma <bulbitain@gmail.com>
2022-08-22 19:42:13 +0900

    solve Topics: Standard output additional contents

---
+++ JetBrainsAcademy/Topics/Standard output/Print code/src/Main.kt
@@ -0,0 +1,8 @@
+fun main() {
+    val s = "fun main() {\n" +
+            "    val one = 1\n" +
+            "    val two = 2\n" +
+            "    println() // empty println\n" +
+            "}"
+    println(s)
+}
