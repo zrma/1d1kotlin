a6c35260d259079c262183f9c8e7ef732c55f2cc
zrma <bulbitain@gmail.com>
2022-09-11 22:32:54 +0900

    solve Stage 1/6: Read the list

--- JetBrainsAcademy/Tasklist/task/src/tasklist/Main.kt
+++ JetBrainsAcademy/Tasklist/task/src/tasklist/Main.kt
@@ -1,7 +1,34 @@
 package tasklist
 
 fun main() {
-    // write your code here
+    val todo = ToDoList()
+
+    print("Input the tasks (enter a blank line to end):")
+    while (true) {
+        val input = readLine()!!.trim()
+        if (input.isEmpty()) {
+            break
+        }
+        todo.add(input)
+    }
+
+    todo.printAll()
 }
 
+class ToDoList {
+    fun printAll() {
+        if (items.isEmpty()) {
+            println("No tasks have been input")
+        } else {
+            items.forEachIndexed() { index, item ->
+                println(String.format("%-2d %s", index + 1, item))
+            }
+        }
+    }
 
+    fun add(input: String) {
+        items.add(input)
+    }
+
+    private val items = mutableListOf<String>()
+}
