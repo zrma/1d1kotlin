2a183edd5a7e828f018ed34aa78e1bbc10d4c967
zrma <bulbitain@gmail.com>
2022-10-05 19:41:51 +0900

    solve Topics: Invoking functions

---
+++ JetBrainsAcademy/Topics/Invoking functions/Lembas/src/Main.kt
@@ -0,0 +1,10 @@
+// don't change the code below      
+fun totalLembas(first: String, second: String) = print(first.toInt() + second.toInt())
+
+fun main() {
+    val breadFromFrodo = readln()
+    val breadFromSam = readln()
+
+    // write your code here
+    totalLembas(breadFromFrodo, breadFromSam)
+}
