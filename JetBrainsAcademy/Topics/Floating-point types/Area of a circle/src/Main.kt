1e5d04540da4eb70e4443af07a860a7cb359ae5b
zrma <bulbitain@gmail.com>
2022-10-07 21:40:53 +0900

    solve Topics: Floating-point types additional contents

---
+++ JetBrainsAcademy/Topics/Floating-point types/Area of a circle/src/Main.kt
@@ -0,0 +1,7 @@
+import kotlin.math.pow
+
+fun main() {
+    val r = readLine()!!.toInt()
+    val pi = 3.1415
+    println((pi * r.toDouble().pow(2.0)))
+}
---
+++ JetBrainsAcademy/Topics/Repeating blocks/Repeat by value/src/Main.kt
@@ -0,0 +1,6 @@
+fun main() {
+    val input = readLine()!!
+    repeat(input.toInt()) {
+        print(input)
+    }
+}
