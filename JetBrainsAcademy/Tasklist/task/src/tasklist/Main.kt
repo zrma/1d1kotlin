1bee84b5c52e83827bf307b277e8731fc0be3c2e
zrma <bulbitain@gmail.com>
2022-11-21 21:35:08 +0900

    solve Stage 5/6: Frame and colors

--- JetBrainsAcademy/Tasklist/task/src/tasklist/Main.kt
+++ JetBrainsAcademy/Tasklist/task/src/tasklist/Main.kt
@@ -3,9 +3,7 @@ package tasklist
 import kotlinx.datetime.*
 import kotlinx.datetime.TimeZone
 import java.time.DateTimeException
-import java.time.Instant
 import java.time.LocalTime
-import java.time.ZoneId
 import java.util.*
 
 fun main() {
@@ -50,6 +48,34 @@ fun main() {
             else -> println("The input action is invalid")
         }
     }
+}
+
+fun initSampleTodo(todo: TaskList) {
+    todo.add(
+        Task(
+            "N", LocalDate(2022, 11, 20), LocalTime.of(19, 0), listOf("Supermarket", "milk", "cookies", "butter")
+        )
+    )
+    todo.add(
+        Task(
+            "N",
+            LocalDate(2022, 11, 21),
+            LocalTime.of(19, 0),
+            listOf("Remember to review the code for the Tasklist", " project.")
+        )
+    )
+    todo.add(
+        Task(
+            "N", LocalDate(2022, 11, 22), LocalTime.of(19, 0), listOf(
+                "Find resources about Ansi colors and cursor ",
+                "movement.",
+                "Don't forget to look into the stage 6 links ",
+                "about it."
+            )
+        )
+    )
+
+    todo.printAll()
 }
 
 fun readPriority(): String {
@@ -108,11 +134,20 @@ fun readTaskBody(): List<String> {
     val res = mutableListOf<String>()
     val scanner = Scanner(System.`in`)
     while (scanner.hasNextLine()) {
-        val input = scanner.nextLine().trim()
+        var input = scanner.nextLine().trim()
         if (input.isEmpty()) {
             break
         }
-        res.add(input)
+
+        val limit = 44
+        while (input.length > limit) {
+            val s = input.take(limit)
+            res.add(s)
+            input = input.drop(limit)
+        }
+        if (input.isNotEmpty()) {
+            res.add(input)
+        }
     }
     return res
 }
@@ -124,9 +159,13 @@ class TaskList {
             return false
         }
 
+        println("+----+------------+-------+---+---+--------------------------------------------+")
+        println("| N  |    Date    | Time  | P | D |                   Task                     |")
+        println("+----+------------+-------+---+---+--------------------------------------------+")
+
         tasks.forEachIndexed() { index, item ->
             item.print(index + 1)
-            println()
+            println("+----+------------+-------+---+---+--------------------------------------------+")
         }
         return true
     }
@@ -206,8 +245,20 @@ class TaskList {
 
 data class Task(var priority: String, var date: LocalDate, var time: LocalTime, var description: List<String>) {
     fun print(idx: Int) {
-        println(String.format("%-2d %s %s %s %s", idx, date, time, priority, overdue()))
-        description.forEach() { println(String.format("%-2s %s", "", it)) }
+        println(
+            String.format(
+                "| %-2d | %s | %s | %s | %s |%-44s|",
+                idx,
+                date,
+                time,
+                priorityToColor(priority),
+                dueToColor(overdue()),
+                description[0]
+            )
+        )
+        description.drop(1).forEach {
+            println(String.format("|    |            |       |   |   |%-44s|", it))
+        }
     }
 
     private fun overdue(): String {
@@ -218,5 +269,22 @@ data class Task(var priority: String, var date: LocalDate, var time: LocalTime, 
             numberOfDays == 0 -> "T"
             else -> "I"
         }
+    }
+}
+
+fun priorityToColor(priority: String): String {
+    return when (priority) {
+        "C" -> "\u001B[101m \u001B[0m"
+        "N" -> "\u001B[102m \u001B[0m"
+        "H" -> "\u001B[103m \u001B[0m"
+        else -> "\u001B[104m \u001B[0m"
+    }
+}
+
+fun dueToColor(due: String): String {
+    return when (due) {
+        "O" -> "\u001B[101m \u001B[0m"
+        "I" -> "\u001B[102m \u001B[0m"
+        else -> "\u001B[103m \u001B[0m"
     }
 }
