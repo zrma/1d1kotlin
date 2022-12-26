48d57a10e0a00f58bafaeb5319df65f11bf3ab7f
zrma <bulbitain@gmail.com>
2022-12-26 14:57:54 +0900

    solve Stage 3/7: Commits

--- JetBrainsAcademy/Git Internals/task/src/gitinternals/Main.kt
+++ JetBrainsAcademy/Git Internals/task/src/gitinternals/Main.kt
@@ -1,6 +1,9 @@
 package gitinternals
 
 import java.io.File
+import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 import java.util.zip.InflaterInputStream
 
 fun main() {
@@ -15,8 +18,7 @@ fun main() {
   val gitObj = readGitObject(gitObjPath)
   val parsed = parseGitObject(gitObj)
 
-  val header = parsed.first()
-  header.split(" ").let { println("type:${it[0]} length:${it[1]}") }
+  printParsedGitObj(parsed)
 }
 
 fun parseGitObject(gitObject: String): List<String> {
@@ -28,4 +30,53 @@ fun readGitObject(location: String): String {
   val bytes = file.readBytes()
   val inflater = InflaterInputStream(bytes.inputStream())
   return inflater.bufferedReader().readText()
+}
+
+fun printParsedGitObj(parsed: List<String>) {
+  val header = parsed.first()
+  val contentType = header.split(" ").first().uppercase()
+  println("*$contentType*")
+
+  var body = parsed.drop(1)
+
+  when (contentType) {
+    "COMMIT" -> {
+      val tree = body.first().split(" ")[1]
+      body = body.drop(1)
+      println("tree: $tree")
+
+      val parents = body.filter { it.startsWith("parent") }
+      body = body.filter { !it.startsWith("parent") }
+      if (parents.isNotEmpty()) {
+        println("parents: ${parents.joinToString(" | ") { it.split(" ").last() }}")
+      }
+
+      val (authorName, authorEmail, authorDate) = parseAuthor(body.first())
+      body = body.drop(1)
+      println("author: $authorName $authorEmail original timestamp: $authorDate")
+
+      val (committerName, committerEmail, committerDate) = parseAuthor(body.first())
+      body = body.drop(1)
+      println("committer: $committerName $committerEmail commit timestamp: $committerDate")
+
+      println("commit message:")
+
+      body.forEach { println(it) }
+    }
+    "BLOB" -> {
+      body.forEach { println(it) }
+    }
+  }
+}
+
+fun parseAuthor(line: String): Triple<String, String, String> {
+  val lines = line.split(" ")
+  val name = lines[1]
+  val email = lines[2].removePrefix("<").removeSuffix(">")
+  val date =
+      LocalDateTime.ofEpochSecond(lines[3].toLong(), 0, ZoneOffset.of(lines[4]))
+          .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")) +
+          " " +
+          ZoneOffset.of(lines[4])
+  return Triple(name, email, date)
 }
